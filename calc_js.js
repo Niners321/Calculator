9f40254f9eb9599e48fb920908d4d76baa5a31d2
Nina <cfratto87@gmail.com>
2022-10-17 11:47:07 -0500

    Initial files and functions

---
+++ calc_js.js
@@ -0,0 +1,19 @@
+//add function
+const add = function(a, b) {
+    return a + b;
+};
+
+//subtract function
+const subtract = function(a, b) {
+    return a - b;
+};
+
+//multiplication function
+const multiply = function(a, b) {
+    return a * b;
+};
+
+//division function
+const divide = function(a, b) {
+    return a / b;
+};
